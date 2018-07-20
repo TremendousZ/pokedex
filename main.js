38f5841407be79f179bfdce8875d089683da6276
John Carlisle <jmcarlisle@live.com>
2018-07-20 10:54:16 -0700

    ran into error when contacting the server, added dummydata - John Carlisle

--- main.js
+++ main.js
@@ -3,29 +3,42 @@ $(document).ready(startUp);
 
 
 function startUp() {
+    $('.enter').on('click' , getPokemonDummyData);
+    // $(".keyPad").on('click', ".keypad > button", enterNumber);
     
 }
 
 
-function getPokemon( pokeNumber){
-    var ajaxCall = {
-        dataType: "json",
-        url: `http://pokeapi.co/api/v2/pokemon/ + ${pokeNumber}`,
-        method: 'get',
-        success: loadPokemonData
+// function getPokemon( ){
+//     console.log("AJAX initiated");
+//     var ajaxCall = {
+//         dataType: "jsonp",
+//         url: `http://pokeapi.co/api/v2/pokemon/56`,
+//         method: 'get',
+//         success: loadPokemonData
 
-    }
+//     }
+//     $.ajax(ajaxCall);
+// }
 
-}
+
+
+
 
 function loadPokemonData( response ) {
    //need picture, number, name, height, weight, ability, type,  
     var nameOfPokemon = response.name;
+    $("#pokemonName").text(nameOfPokemon);
     var pokemonWeight = response.weight;
+    $('#weight').text(pokemonWeight);
     var pokemonImage = response.sprites.front_default; 
+    $('#pokemonImage').src(pokemonImage);
     var pokemonHeight = response.height;
+    $('#height').text(pokemonHeight);
     var pokeNumber = response.id;
+    $('#pokeNumber').text(pokeNumber);
 }
 
-http://pokeapi.co/api/v2/pokemon/ <------number
+// http://pokeapi.co/api/v2/pokemon/ <------number
+
 
