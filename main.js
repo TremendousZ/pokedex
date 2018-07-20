ce68c47ec8cd82b09a4927f12286739e608c2181
John Carlisle <jmcarlisle@live.com>
2018-07-19 23:37:21 -0700

    sucessfully connected with AJAX call and mapped out dtat needed - John Carlisle

--- main.js
+++ main.js
@@ -7,5 +7,25 @@ function startUp() {
 }
 
 
+function getPokemon( pokeNumber){
+    var ajaxCall = {
+        dataType: "json",
+        url: `http://pokeapi.co/api/v2/pokemon/ + ${pokeNumber}`,
+        method: 'get',
+        success: loadPokemonData
+
+    }
+
+}
+
+function loadPokemonData( response ) {
+   //need picture, number, name, height, weight, ability, type,  
+    var nameOfPokemon = response.name;
+    var pokemonWeight = response.weight;
+    var pokemonImage = response.sprites.front_default; 
+    var pokemonHeight = response.height;
+    var pokeNumber = response.id;
+}
+
 http://pokeapi.co/api/v2/pokemon/ <------number
 
